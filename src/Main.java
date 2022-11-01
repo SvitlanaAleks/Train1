5c704c202bc0c21010f1937f707111f5782b664f
SvitlanaAleks <svitlanaalex8@gmail.com>
2022-11-01 21:27:18 +0200

    new objects

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         Train train1=new Train();
+        Train train=new Train();
+        Train train2=new Train();
+
+
+
 
 
 
